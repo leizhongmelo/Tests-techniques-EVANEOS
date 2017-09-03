507f26558440384ee295ee29593c1fe949233b74
changeme <changeme@changeme>
2017-09-03 15:39:32 +0200

    Test Front--Ready to creat a function ShowOccurence

---
+++ Test_Front_JS/Count_Occurrence_Function.js
@@ -0,0 +1,13 @@
+//"On cherche à calculer le nombre d'occurence de chaque mots dans un texte. Les mots de moins de deux lettres seront ignorés. Concevez un module JavaScript qui répond à ce problème. Dans le doute, utilisez votre bon sens.”
+
+//creat a javascript module
+(function(){
+
+//creat a showOccurence function
+function showOccurence(){
+
+}
+
+
+
+}());
